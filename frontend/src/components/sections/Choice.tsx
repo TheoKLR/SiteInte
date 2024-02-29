c455d538d1627b74da3289e121076b01890dc6f9
TheoKLR <theo.koehler@utt.fr>
2024-02-29 13:20:43 +0100

    envoi correct des desires

--- frontend/src/components/sections/Choice.tsx
+++ frontend/src/components/sections/Choice.tsx
@@ -1,5 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './ChoiceStyle.css';
+import { createDesire, getAllDesires, getAllUsers, submitChoices } from '../../services/requests';
+
 
 export const Choice = () => {
 
@@ -18,23 +20,49 @@ export const Choice = () => {
 
     function handleSubmit(event: React.FormEvent){
         console.log(checkedValues);
+        const token = localStorage.getItem("authToken");
+        if (token !== null){
+            console.log("Token :" + token);
+            submitChoices(checkedValues);
+        }
     }
+
+    // const getrole = async() => {
+    //     let response = await api.get('/auth/role')
+    //     return response.data
+    // }
+
+    // const role = getrole();
+    // console.log(role);
+    // const response = createDesire('Dev', 'Comme Théo et Lou');
+    // console.log(response)
+
+    const [desires, setDesires] = useState<any[]>([]);
+
+    useEffect(() => {
+        getAllDesires()
+        .then(res => {
+            const data = res.data;
+            setDesires(data);
+        })
+        .catch(error => {
+            console.error('Une erreur s\'est produite lors de la récupération des désirs :', error);
+        });
+    }, [])    
 
     return (
         <>
             <div className='containerChoix'>
                 <p>L'inté a besoin de beaucoup de gens dans de nombreux domaines. Choisis ton rôle afin d'aider au mieux!</p><br />
                 <div className='inputs'>
-                    <input type="checkbox" value="1" onChange={handleChange}/> 1
-                    <br />
-                    <input type="checkbox" value="2" onChange={handleChange}/> 2
-                    <br />
-                    <input type="checkbox" value="3" onChange={handleChange}/> 3 
-                    <br />
+                {desires.map((desire, index) => (
+                    <div key={index}>
+                        <input type="checkbox" value={desire.id} onChange={handleChange}/> {desire.name}
+                    </div>
+                ))}
                     <button onClick={handleSubmit}>Valider</button>
                 </div>
             </div>
-
         </>
     )
 }
--- frontend/src/services/requests.ts
+++ frontend/src/services/requests.ts
@@ -2,11 +2,38 @@ import { api } from './api';
 
 
 export const getAllUsers = async() => {
-    const response = await api.get('user/all')
+    let response = await api.get('user/all')
     return response.data
 }
 
 export const getAllTeams = async() => {
     const response = await api.get('team/all')
     return response.data
+}
+
+export const getAllDesires = async() => {
+    let response = await api.get('desire/all')
+    return response.data
+}
+
+export const createDesire = async (name: string, desc: string) => {
+    try {
+        const response = await api.post('/desire/', { name, desc });
+        return response.data; 
+    } catch (error) {
+        console.error('Erreur lors de l\'enregistrement du desire :', error);
+        throw error;
+    }
+};
+    
+
+export const submitChoices = async(choices:number[]) => {
+    try {
+        const response = await api.post('/desire/submit',
+            { desireIds: choices},
+        );
+        console.log(response.data);
+    } catch (err: any) {
+        console.error(err);
+    }
 }
