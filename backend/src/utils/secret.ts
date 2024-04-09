079ee35f8db9657f77c2caab0c28f940bfc2ba84
lfcm64 <lou.morale@utt.fr>
2024-04-09 21:52:16 +0200

    preparing v1.0-prod

--- backend/src/utils/secret.ts
+++ backend/src/utils/secret.ts
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 dotenv.config()
 
 export const jwtSecret = process.env.JWT_SECRET || 'default';
-export const dev_db_url = process.env.OUTSIDE_DATABASE_URL;
+export const dev_db_url = process.env.DATABASE_URL;
 export const api_client = process.env.ETU_UTT_CLIENT_ID || 'default';
 export const api_secret = process.env.ETU_UTT_CLIENT_SECRET || 'default';
