3d432154ec7f8ffb72224bd2dc8691f140457ece
lfcm64 <lou.morale@utt.fr>
2024-04-09 23:36:18 +0200

    preparing v1.0-prod

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -7,6 +7,6 @@ export default {
   out: "./drizzle",
   driver: "pg",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL as string,
+    connectionString: process.env.OUTSIDE_DATABASE_URL as string,
   },
 } satisfies Config;
