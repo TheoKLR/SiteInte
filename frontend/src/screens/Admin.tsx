8e71b0879945d45df62379abb00e1f2252c72ec5
Maxime Letoffet <maxime.letoffet@gmail.com>
2024-07-08 14:07:26 +0200

    Minor updates

--- frontend/src/screens/Admin.tsx
+++ frontend/src/screens/Admin.tsx
@@ -5,19 +5,24 @@ import FactionAdminSection from '../components/admin/factions/FactionAdminSectio
 import DesireAdminSection from '../components/admin/roles/RoleAdminSection';
 import EventsAdminSection from '../components/admin/events/EventsAdminSection'
 import { Section } from "../components/shared/Section";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getRole } from "../services/requests";
 import ExportAdminSection from "../components/admin/export/ExportAdminSection";
 
 export const Admin = () => {
+
+    const [role, setRole] = useState<string | null>(null);
+
     useEffect(() => {
         const fetchRole = async () => {
             try {
                 const role = await getRole();
-                if (role !== "Admin") {
+                if (role !== ("Admin"|| "RespoCE")) {
                     window.location.href = '/Home';
                     return null;
                 }
+                setRole(role);
+                
             } catch (error) {
                 console.error('Error fetching role:', error);
             }
@@ -29,12 +34,12 @@ export const Admin = () => {
     return (
         <div className="Admin">
             <Navbar />
-            <Section titre="Utilisateurs" contenu={UserAdminSection} />
-            <Section titre="Equipes" contenu={TeamAdminSection} />
-            <Section titre="Factions" contenu={FactionAdminSection} />
-            <Section titre="Rôles" contenu={DesireAdminSection} />
-            <Section titre="Events" contenu={EventsAdminSection} />
-            <Section titre="Exports" contenu={ExportAdminSection} />
+            {(role === 'Admin') && <Section titre="Utilisateurs" contenu={UserAdminSection} />}
+            {(role === 'Admin' || 'RespoCE') && <Section titre="Equipes" contenu={TeamAdminSection} />}
+            {(role === 'Admin' || 'RespoCE') && <Section titre="Factions" contenu={FactionAdminSection} />}
+            {(role === 'Admin') &&<Section titre="Rôles" contenu={DesireAdminSection} />}
+            {(role === 'Admin') && <Section titre="Events" contenu={EventsAdminSection} />}
+            {(role === 'Admin') && <Section titre="Exports" contenu={ExportAdminSection} />}
         </div>
     )
 }
