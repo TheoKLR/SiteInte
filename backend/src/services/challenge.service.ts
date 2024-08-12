f7466aa797c86b75bce0afa8f71a5a34553d3824
Hugo Bergerat <hugo.bergerat2002@gmail.com>
2024-08-12 10:20:54 +0200

    update

--- backend/src/services/challenge.service.ts
+++ backend/src/services/challenge.service.ts
@@ -3,11 +3,12 @@ import {
     challenge,
     factionTochallengeSchema,
     factionTochallenge,
-    ChallengeType
+    ChallengeType, StudentTochallengeSchema
 } from "../schemas/challenge.schema"
 import {addPoints, removePoints} from "./faction.service"
 import { db } from "../database/db"
 import {and, eq} from 'drizzle-orm'
+import {userSchema} from "../schemas/user.schema";
 
 export const getAllChallenges = async () => {
     try {
@@ -57,11 +58,46 @@ export const validateChallenge = async (challengeId: number, associatedId: numbe
     //First get the type of challenge
     const challenge = await db.select().from(challengeSchema).where(eq(challengeSchema.id, challengeId));
     if(!challenge || challenge.length === 0) throw new Error("No challenge with id '" + challengeId + "'")
-    const challType = challenge[0].challType
+    const challType: ChallengeType = challenge[0].challType
+    switch (challType) {
+        case ChallengeType.Student:
+            //means associatedId is a student
+            //checking user exist
+            const user = await db.select().from(userSchema).where(eq(userSchema.id, associatedId));
+            if(!user || user.length === 0) throw new Error("No user with id '" + associatedId + "'")
+            //check user is not CE
+            if(user[0].permission === )
+            //Associating the challenge to the user
+            //checking is event already associated with this user
+            const associatedChall = await db.select().from(StudentTochallengeSchema)
+                .where(
+                    and(
+                        eq(StudentTochallengeSchema.challengeId, challengeId),
+                        eq(StudentTochallengeSchema.studentId, associatedId)
+                    )
+                );
+
+            if(associatedChall) throw new Error("Event already associated with this user. userId '" + associatedId + "'")
+            //associating the event
+            await db.insert(StudentTochallengeSchema).values({
+                challengeId: challengeId,
+                studentId: associatedId,
+                attributedPoints: attributedPoints
+            }).execute();
+            break;
+        case ChallengeType.StudentOrCe:
+            break;
+        case ChallengeType.Team:
+            break;
+        case ChallengeType.Faction:
+            break;
+        case ChallengeType.Free:
+            break;
+    }
     const newFacToChall: factionTochallenge = { challengeId, factionId: associatedId, attributedPoints};
     const alreadyValidate = await db.select().from(factionTochallengeSchema).where(eq(factionTochallengeSchema, newFacToChall));
     //if(alreadyValidate.length > 0) throw new Error(`Challenge ${challengeId} already validated for team ${factionId}.`)
-    if (alreadyValidate.length === 0) {
+    if (!alreadyValidate || alreadyValidate.length === 0) {
         await addPoints(associatedId, attributedPoints);
         await db.insert(factionTochallengeSchema).values(newFacToChall);
     }
