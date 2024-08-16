7999cf9ad03f6ea67a2a78636b01b3299d64a95e
Hugo Bergerat <hugo.bergerat2002@gmail.com>
2024-08-16 23:04:24 +0200

    choses obscures avec la db

--- backend/drizzle/0006_huge_pride.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE "userToRole" DROP CONSTRAINT "userToRole_user_id_user_id_fk";
---> statement-breakpoint
-ALTER TABLE "user" ADD COLUMN "birthday" date;--> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "userToRole" ADD CONSTRAINT "userToRole_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE cascade ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
--- backend/drizzle/0009_awesome_vermin.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE "newstudentUUID" RENAME TO "newstudent";--> statement-breakpoint
-ALTER TABLE "newstudent" RENAME COLUMN "uiid" TO "uuid";--> statement-breakpoint
-ALTER TABLE "newstudent" DROP CONSTRAINT "newstudentUUID_user_id_user_id_fk";
---> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "newstudent" ADD CONSTRAINT "newstudent_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
