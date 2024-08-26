67e0824da6821c0ddd7ef5e063f1e4bffeb58214
Maxime Letoffet <maxime.letoffet@gmail.com>
2024-08-26 16:36:04 +0200

    perm fix 24h before

--- backend/src/utils/time_utils.ts
+++ backend/src/utils/time_utils.ts
@@ -30,3 +30,9 @@ export const timeToStr = async (startTime: any, endTime: any) => {
 
     return { start_time_str, end_time_str };
 };
+
+  // Fonction pour parser une date au format "dd/MM/yyyy HH:mm:ss"
+ export const parseDateString = (dateString: string): Date => {
+    const [day, month, year, hour, minute, second] = dateString.split(/[\s/:]+/).map(Number);
+    return new Date(Date.UTC(year, month - 1, day, hour, minute, second));
+  };
