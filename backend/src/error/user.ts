d88bd581effb4750e6189de86b8cca90bbb12484
Hugo Bergerat <hugo.bergerat2002@gmail.com>
2024-08-29 14:21:11 +0200

    fiw wrong error essage.

--- backend/src/error/user.ts
+++ backend/src/error/user.ts
@@ -2,4 +2,4 @@
 
 export const NoUser = (id: number) => new Error("No user with id '" + id + "'")
 export const NoTeam = (id: number | null) => new Error("No team with id '" + id + "'")
-export const NoFaction = (id: number) => new Error("No faction with id '" + id + "'")
+export const NoFaction = (id: number | null) => new Error("No faction with id '" + id + "'")
