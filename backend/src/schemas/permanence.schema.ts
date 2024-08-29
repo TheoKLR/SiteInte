3215ebaded878aea41848f377cee5c8f170ce6de
Hugo Bergerat <hugo.bergerat2002@gmail.com>
2024-08-29 09:27:52 +0200

    Delete fucking column id.

--- backend/src/schemas/permanence.schema.ts
+++ backend/src/schemas/permanence.schema.ts
@@ -1,5 +1,6 @@
 import { pgTable, serial, text, integer, date, boolean } from "drizzle-orm/pg-core";
 import { userSchema } from "./user.schema";
+import { primaryKey } from 'drizzle-orm/pg-core';
 
 export const permanenceSchema = pgTable("Permanence", {
   id: serial("id").primaryKey(),
@@ -14,11 +15,12 @@ export const permanenceSchema = pgTable("Permanence", {
 
 // Schéma des inscriptions
 export const registrationSchema = pgTable("Registration", {
-  id: serial("id").primaryKey(),
   userId: integer("user_id").references(() => userSchema.id, { onDelete: "cascade" }).notNull(),
   permanenceId: integer("permanence_id").references(() => permanenceSchema.id, { onDelete: "cascade" }).notNull(),
   registeredAt: date("registered_at").defaultNow(),
-});
+}, (table) => ({
+  pk: primaryKey(table.userId, table.permanenceId) // Clé primaire composite
+}))
 
 
 export type Permanence = typeof permanenceSchema.$inferInsert;
