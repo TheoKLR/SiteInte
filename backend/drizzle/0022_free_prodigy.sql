712846166f59710a23ae86e032cfcf89602ab384
Maxime Letoffet <maxime.letoffet@gmail.com>
2024-09-02 16:08:35 +0200

    gi_id fix

--- backend/drizzle/0022_free_prodigy.sql
+++ backend/drizzle/0022_free_prodigy.sql
@@ -7,7 +7,7 @@ ALTER TABLE "team" RENAME COLUMN "city_id" TO "faction_id";--> statement-breakpo
 ALTER TABLE "team" DROP CONSTRAINT "team_city_id_faction_id_fk";
 --> statement-breakpoint
 ALTER TABLE "Registration" ADD CONSTRAINT "Registration_user_id_permanence_id_pk" PRIMARY KEY("user_id","permanence_id");--> statement-breakpoint
-ALTER TABLE "team" ADD COLUMN "gi_id" text;--> statement-breakpoint
+ALTER TABLE "team" ADD COLUMN "gi_id" integer;--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "team" ADD CONSTRAINT "team_faction_id_faction_id_fk" FOREIGN KEY ("faction_id") REFERENCES "faction"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
